28133019adbbf9af3a72352e4d14f9e5828b8cb4
Renato Gama <renatoargh@gmail.com>
2013-06-13 23:58:29 -0300

    some more testing for math.mod

--- tests/mathUtilsTest.js
+++ tests/mathUtilsTest.js
@@ -3,6 +3,7 @@ var mathUtils = require("../mathUtils");
 module.exports = {
 	"mod: Check that it calculates the right values": function(test){
 		test.equal(mathUtils.mod("036532", [2, 3, 4, 5, 6, 7]), 4);
+		test.equal(mathUtils.mod("347389", [2, 3, 4, 5, 6, 7]), 2);
 		test.done();
 	}
 };
