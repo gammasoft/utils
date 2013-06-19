f2ddb9c5220ff85f1e13e12bfd9e5087d25745b2
Renato Gama <renatoargh@gmail.com>
2013-06-19 15:38:00 -0300

    added index file/tests and changed package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	array: require("./arrayUtils"),
+	console: require("./consoleUtils"),
+	crypto: require("./cryptoUtils"),
+	math: require("./mathUtils"),
+	number: require("./numberUtils"),
+	object: require("./objectUtils"),
+	string: require("./stringUtils"),
+};
---
+++ tests/indexTest.js
@@ -0,0 +1,15 @@
+var utils = require("../index");
+
+module.exports = {
+	"Checks that every submodule is available": function(test){
+		test.ok(utils.array);
+		test.ok(utils.console);
+		test.ok(utils.crypto);
+		test.ok(utils.math);
+		test.ok(utils.number);
+		test.ok(utils.object);
+		test.ok(utils.string);
+		
+		test.done();
+	}
+};
