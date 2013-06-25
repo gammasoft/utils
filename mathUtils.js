1d9c7800fa14e90595579c816723ad1a6a274abc
Renato Gama <renatoargh@gmail.com>
2013-06-25 09:55:43 -0300

    implemented convolve/multiplyPolynomials and tests

--- mathUtils.js
+++ mathUtils.js
@@ -11,3 +11,27 @@ module.exports.mod = function(value, factors, divider){
 		return (factors[i++] * parseInt(current, 10)) + last; 
 	}, 0) % divider;
 };
+
+module.exports.convolve = convolve;
+module.exports.multiplyPolynomials = convolve;
+
+function convolve(signal1, signal2){
+    var signal1Length = signal1.length;
+    var signal2Length = signal2.length;
+    var finalSize = signal1Length + signal2Length - 1;
+
+    var result = new Array(finalSize);
+
+    for (var i = 0; i < finalSize; i++) {
+        result[i] = 0;
+
+        for (var j = 0; j < signal2Length; j++) {
+            if(i - j < 0) continue;
+            if(i - j >= signal1Length) continue;
+
+            result[i] += signal2[j] * signal1[i - j];
+        }
+    }
+
+    return result;
+}
--- tests/mathUtilsTest.js
+++ tests/mathUtilsTest.js
@@ -5,5 +5,25 @@ module.exports = {
 		test.equal(mathUtils.mod("036532", [2, 3, 4, 5, 6, 7]), 4);
 		test.equal(mathUtils.mod("347389", [2, 3, 4, 5, 6, 7]), 2);
 		test.done();
+	},
+	
+	"convolve: Check that convolves properly": function(test){
+		test.deepEqual(mathUtils.convolve([1, 1, 1], [0.5, 2, 2.5, 1]), [0.5, 2.5, 5, 5.5, 3.5, 1]);
+		test.deepEqual(mathUtils.convolve([3, 4, 5], [2, 1]), [6, 11, 14, 5]);
+		test.deepEqual(mathUtils.convolve([2, -2, 1], [1, 3, 0.5, -1]), [2, 4, -4, 0, 2.5, -1]);
+		test.deepEqual(mathUtils.convolve([1, 2, 3, 4], [-1, 5, 3]), [-1, 3, 10, 17, 29, 12]);
+		test.deepEqual(mathUtils.convolve([1, 2], [2, 1, 1, 1]), [2, 5, 3, 3, 2]);
+		test.deepEqual(mathUtils.convolve([1, 2, 3, 4, 5], [1]), [1, 2, 3, 4, 5]);
+		test.done();
+	},
+	
+	"multiplyPolynomials: Check that polynomials are multiplied properly": function(test){
+		test.deepEqual(mathUtils.multiplyPolynomials([1, 1, 1], [0.5, 2, 2.5, 1]), [0.5, 2.5, 5, 5.5, 3.5, 1]);
+		test.deepEqual(mathUtils.multiplyPolynomials([3, 4, 5], [2, 1]), [6, 11, 14, 5]);
+		test.deepEqual(mathUtils.multiplyPolynomials([2, -2, 1], [1, 3, 0.5, -1]), [2, 4, -4, 0, 2.5, -1]);
+		test.deepEqual(mathUtils.multiplyPolynomials([1, 2, 3, 4], [-1, 5, 3]), [-1, 3, 10, 17, 29, 12]);
+		test.deepEqual(mathUtils.multiplyPolynomials([1, 2], [2, 1, 1, 1]), [2, 5, 3, 3, 2]);
+		test.deepEqual(mathUtils.multiplyPolynomials([1, 2, 3, 4, 5], [1]), [1, 2, 3, 4, 5]);
+		test.done();
 	}
 };
