71d5441a3b9673c04203b52f4ac6aef886ac18db
Renato Gama <renatoargh@gmail.com>
2014-02-11 23:10:33 -0200

    Added cryptoUtils.sha1 and cryptoUtils.hmac. Version bump.

--- tests/cryptoUtilsTest.js
+++ tests/cryptoUtilsTest.js
@@ -13,6 +13,18 @@ function testForCipherAndDecipher(test){
 }
 
 module.exports = {
+    'hmac': {
+        //too random to test
+    },
+
+    'sha1': {
+        'Check that sha1 is properly calculated': function(test){
+            test.ok(cryptoUtils.sha1('gammautils'), '87d70d555f99168adcb987e0ce6a29f4aa7de885');
+            test.ok(cryptoUtils.sha1('Gammasoft Desenvolvimento de Software Ltda'), 'ec4748010cca99c353e32dfc1b03baa9625713b6');
+            test.done();
+        }
+    },
+
     'md5': {
         'Check that md5 is properly calculated': function(test){
             test.ok(cryptoUtils.md5('gammautils'), '5c46d1c40197fdea3fc01dfca60a6b4e');
